3231b8599977b288954c5f65d72605ef56bd649c
hope <Lul-Abdifan>
2023-03-05 15:57:42 -0500

    Add hover effect on Navbar title

--- blog/blog/src/Navbar.js
+++ blog/blog/src/Navbar.js
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return ( 
         <nav class="flex justify-center justify-around p-10">
-            <h1 class="text-blue-600 text-bold text-2xl hover:animate-spin text-left">Blogger</h1>
+            <h1 class="text-4xl text-4xl hover:animate-bounce transition duration-700 text-red-500">Blogger</h1>
             <div class="text-2xl text-fuchsia-700 font-semibold">
               <ul class="flex gap-6 ">
                 <li>About</li>
