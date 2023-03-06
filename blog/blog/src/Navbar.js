474853d930ef93ce3eb58f8626ccbecfe27bad7e
hope <tasanbannu@gmail.com>
2023-03-06 01:57:53 -0500

    Add functionality to the blog

--- blog/blog/src/Navbar.js
+++ blog/blog/src/Navbar.js
@@ -3,11 +3,11 @@ const Navbar = () => {
         <nav class="flex justify-center justify-around p-10">
             <h1 class="text-4xl text-4xl hover:animate-bounce transition duration-700 text-red-500">Blogger</h1>
             <div class="text-2xl text-fuchsia-700 font-semibold">
-              {/* <ul class="flex gap-6 ">
+              <ul class="flex gap-6 ">
                 <li>About</li>
                 <li>Home</li>
                 <li>New Blog</li>
-              </ul> */}
+              </ul>
 
 
             </div>
