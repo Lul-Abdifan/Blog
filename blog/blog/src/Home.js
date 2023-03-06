eefbd72fb830cb0a660a677280ec56d7becef383
hope <tasanbannu@gmail.com>
2023-03-06 03:10:08 -0500

    Install tailwind css for styling

--- blog/blog/src/Home.js
+++ blog/blog/src/Home.js
@@ -1,17 +1,18 @@
 import { useState } from "react";
+import Lists from "./Lists";
 const Home = () =>
  {
     const [blog,setBlog]=useState(
     [
         {title:"Hope",
         Author:"Abdi",
-        body:"lorem..............................        ",
+        body:"lorem.................fff.............        ",
         id:1
        },
        {title:"Given Life ",
        Author:"Abinat",
        body:"lorem.rrrrrsssssssssssssssssssrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr..............................        ",
-       id:1   
+       id:2 
        },
        {title:"In christ",
         Author:"Naol",
@@ -21,19 +22,7 @@ const Home = () =>
     ])
     return ( 
 <div>
-  {
-    blog.map((list)=>(
-     
-<div class="flex items-center justify-center mt-5">
-  <div class="bg-gray-300 p-5 rounded-lg mt-2 w-300">
-        <h1 class="font-bold text-2xl bg-purple text-red-600 ">{list.title}</h1>
-        <h2 class="text-red-500 text-bold pb-2">Written By {list.Author}</h2>
-        <p class="w-80 text break-words">{list.body}</p>
-       </div>
-       </div>
-    ))
-  }
-
+  <Lists lists={blog}/>
 
 </div>
      );
